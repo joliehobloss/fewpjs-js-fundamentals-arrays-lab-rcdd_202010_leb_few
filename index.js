f1a6dc109edefcf078e4775ba9960ded847fbc6d
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-12 16:53:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution here!
 const append = ["Milo", "Otis", "Garfield"];
-const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
 const append = ["Milo", "Otis", "Garfield"];
 const append = ["Milo", "Otis", "Garfield"];
